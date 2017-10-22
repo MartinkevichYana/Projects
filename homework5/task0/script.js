d03d6524c5b053a1ac99635acf1ea72cb8f0e454
Martinkevich Yana Stanislavovna <Yana22685@yandex.ru>
2017-10-22 23:41:13 +0300

    hw5

--- homework5/task0/script.js
+++ homework5/task0/script.js
@@ -2,87 +2,31 @@ var t=0;
 var tx=0;
 var com=0;
 
-function teg(child) {
-	if(child.hasChildNodes())
+function func(el) {
+	if(el.nodeType==1)
 	{
-		for (i = 0; i < child.childNodes.length; i++)
+		t++;
+	}
+	if(el.nodeType==3)
+	{
+		tx++;
+	}
+	if(el.nodeType==8)
+	{
+		com++;
+	}
+	if(el.hasChildNodes())
+	{
+		for (var i = 0; i < el.childNodes.length; i++)
 		{
-
-			if (child.childNodes[i].nodeType == 1 )
-            {
-            	if (child.childNodes[i].hasChildNodes())
-            	{
-            		t++;
-            		teg(child.childNodes[i]);
-            	}
-            }
+			func(el.childNodes[i]);
 		}
 	}
-	return t;
 }
 
-function text(child) {
-	if(child.hasChildNodes())
-	{
-		for (i = 0; i < child.childNodes.length; i++)
-		{
+var el=document.getElementsByTagName('html')[0];
+func(el);
 
-			if (child.childNodes[i].nodeType == 3 )
-            {
-            	if (child.childNodes[i].hasChildNodes())
-            	{
-            		tx++;
-            		text(child.childNodes[i]);
-            	}
-            }
-		}
-	}
-	return tx;
-}
-
-function comment(child) {
-	if(child.hasChildNodes())
-	{
-		for (i = 0; i < child.childNodes.length; i++)
-		{
-
-			if (child.childNodes[i].nodeType == 8 )
-            {
-            	if (child.childNodes[i].hasChildNodes())
-            	{
-            		com++;
-            		comment(child.childNodes[i]);
-            	}
-            }
-		}
-	}
-	 return com;
-}
-
-var child=document.getElementsByTagName('html')[0];
-alert("teg "+teg(child));
-alert("text "+text(child));
-alert("comment"+comment(child));
-function comment(child) {
-	if(child.hasChildNodes())
-	{
-		for (i = 0; i < child.childNodes.length; i++)
-		{
-
-			if (child.childNodes[i].nodeType == 8 )
-            {
-            	com+=child.childNodes.length;
-            	if (child.childNodes[i].hasChildNodes())
-            	{
-            		comment(child.childNodes[i]);
-            	}
-            }
-		}
-	}
-	 alert("comment " + com);
-}
-
-var child=document.getElementsByTagName('html')[0];
-teg(child);
-text(child);
-comment(child);
+alert("teg "+t);
+alert("text "+tx);
+alert("comment "+com);
