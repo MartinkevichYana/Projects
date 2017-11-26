2940b3daded5b31d5d9676a7cdf7c68316d4dd99
Martinkevich Yana Stanislavovna <Yana22685@yandex.ru>
2017-11-27 00:42:52 +0300

    hw13

--- homework13/script.js
+++ homework13/script.js
@@ -9,7 +9,10 @@ var line = document.getElementsByTagName("button")[3];
 var color = document.getElementsByTagName("button")[4];
 var back = document.getElementsByTagName("button")[5];
 var link = document.getElementsByTagName("button")[6];
-var clear = document.getElementsByTagName("button")[7];
+var rig = document.getElementsByTagName("button")[7];
+var cent = document.getElementsByTagName("button")[8];
+var left = document.getElementsByTagName("button")[9];
+var clear = document.getElementsByTagName("button")[10];
 var sfont = document.getElementsByTagName("select")[0];
 var ssize = document.getElementsByTagName("select")[1];
 var list = document.getElementsByTagName("select")[2];
@@ -18,45 +21,45 @@ var img = document.getElementsByTagName("img");
 dtext.contentEditable = true;
 var text = dtext.innerHTML;
 var str = "";
-//сделать функцию для проверки если текст не выделен то сделать для всего текста
+
 bold.addEventListener("click", function(e) {
 	str = window.getSelection().toString();
 	document.execCommand ('bold', false, null);
-	bold.classList.toggle("sost");
 });
 it.addEventListener("click", function(e) {
 	str = window.getSelection().toString();
 	document.execCommand ('italic', false, null);
-	it.classList.toggle("sost");
 });
 und.addEventListener("click", function(e) {
 	str = window.getSelection().toString();
 	document.execCommand ('underline', false, null);
-	und.classList.toggle("sost");
 });
 sfont.addEventListener("change", function() {
 	document.execCommand ('fontname', false, sfont.value);
-	sfont.classList.add("sost");
 });
 ssize.addEventListener("change", function() {
 	document.execCommand ('fontsize', false, ssize.value);
-	ssize.classList.add("sost");
 });
 line.addEventListener("click", function() {
 	document.execCommand ('inserthorizontalrule', false, null);
-	line.classList.toggle("sost");
 });
 list.addEventListener("change", function() {
 	document.execCommand (list.value, false, null);
-	list.classList.add("sost");
 });
 color.addEventListener("click", function() {
 	dcol.classList.toggle("color");
-	color.classList.toggle("sost");
 });
 back.addEventListener("click", function() {
 	dback.classList.toggle("back");
-	back.classList.toggle("sost");
+});
+rig.addEventListener("click", function(e) {
+	document.execCommand ('justifyright', false, null);
+});
+cent.addEventListener("click", function(e) {
+	document.execCommand ('justifycenter', false, null);
+});
+left.addEventListener("click", function(e) {
+	document.execCommand ('justifyleft', false, null);
 });
 
 for(var i=0; i<dcol.children.length; i++)
@@ -82,14 +85,93 @@ function imgback(i) {
 }
 link.addEventListener("click", function() {
 	document.execCommand ("createlink", false, inplink.value);
-	link.classList.toggle("sost");
 });
 clear.addEventListener("click", function() {
 	dtext.innerHTML = text;
-	clear.classList.toggle("sost");
-	var c = document.getElementsByClassName("sost");
-	for(var i=0; i<c.length; i++)
+});
+dtext.addEventListener("mouseup", function() {
+	if(document.queryCommandState("bold")==true)
 	{
-		c[i].classList.remove("sost");
+		bold.classList.add("sost");
+	}
+	if(document.queryCommandState("italic")==true)
+	{
+		it.classList.add("sost");
+	}
+	if(document.queryCommandState("underline")==true)
+	{
+		und.classList.add("sost");
+	}
+	if(document.queryCommandState("fontname")==true)
+	{
+		sfont.classList.add("sost");
+	}
+	if(document.queryCommandState("inserthorizontalrule")==true)
+	{
+		line.classList.add("sost");
+	}
+	if(document.queryCommandState("forecolor")==true)
+	{
+		color.classList.add("sost");
+	}
+	if(document.queryCommandState("backcolor")==true)
+	{
+		back.classList.add("sost");
+	}
+	if(document.queryCommandState("justifyright")==true)
+	{
+		rig.classList.add("sost");
+	}
+	if(document.queryCommandState("justifycenter")==true)
+	{
+		cent.classList.add("sost");
+	}
+	if(document.queryCommandState("justifyleft")==true)
+	{
+		left.classList.add("sost");
+	}
+	if(document.queryCommandState("bold")==false)
+	{
+		bold.classList.remove("sost");
+	}
+	if(document.queryCommandState("italic")==false)
+	{
+		it.classList.remove("sost");
+	}
+	if(document.queryCommandState("underline")==false)
+	{
+		und.classList.remove("sost");
+	}
+	if(document.queryCommandState("fontname")==false)
+	{
+		sfont.classList.remove("sost");
+	}
+	if(document.queryCommandState("fontsize")==false)
+	{
+		ssize.classList.remove("sost");
+	}
+	if(document.queryCommandState("inserthorizontalrule")==false)
+	{
+		line.classList.remove("sost");
+	}
+	if(document.queryCommandState("forecolor")==false)
+	{
+		color.classList.remove("sost");
+	}
+	if(document.queryCommandState("backcolor")==false)
+	{
+		back.classList.remove("sost");
+	}
+	if(document.queryCommandState("justifyright")==false)
+	{
+		rig.classList.remove("sost");
+	}
+	if(document.queryCommandState("justifycenter")==false)
+	{
+		cent.classList.remove("sost");
+	}
+	if(document.queryCommandState("justifyleft")==false)
+	{
+		left.classList.remove("sost");
 	}
 });
