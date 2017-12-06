50f4c7df0c38ae5f4c251c167396ec2013fd9b54
Martinkevich Yana Stanislavovna <Yana22685@yandex.ru>
2017-12-07 00:45:48 +0300

    hw18

---
+++ homework18/script.js
@@ -0,0 +1,117 @@
+var chb = document.getElementsByTagName("button")[0];
+var neg = document.getElementsByTagName("button")[1];
+var cr = document.getElementsByTagName("button")[2];
+var d = document.getElementsByTagName("div")[0];
+var img = document.getElementById('img');
+var can = document.getElementById('c');
+var ctx = can.getContext('2d');
+ctx.drawImage(img, 0, 0);
+var k = 0;
+
+can.addEventListener("dragenter", function(e) {
+	this.innerText = "";
+});
+can.addEventListener("dragleave", function(e) {
+	this.innerText = "";
+});
+can.addEventListener("dragover", function(e) {
+	e.preventDefault();
+});
+can.addEventListener("drop", function(e) {
+	e.preventDefault();
+	ctx.clearRect(0, 0, 1500, 500);
+	var dT = e.dataTransfer;
+	var fileReader = new FileReader();
+	fileReader.onload = function() {
+		img.src = this.result;
+		ctx.drawImage(img, 0, 0);
+		if(k == 1)
+		{
+			var idata = ctx.getImageData(0, 0, 1000, 500);
+			var idataf = blwh(idata);
+			ctx.putImageData(idataf, 0, 0);
+		}
+		if(k == 2)
+		{
+			var idata = ctx.getImageData(0, 0, 1000, 500);
+			var idataf = negativ(idata);
+			ctx.putImageData(idataf, 0, 0);
+		}
+		if(k == 3)
+		{
+			var idata = ctx.getImageData(0, 0, 1000, 500);
+			var idataf = coar(idata);
+			ctx.putImageData(idataf, 0, 0);
+		}
+		
+	}
+	fileReader.readAsDataURL(dT.files[0]);
+});
+
+chb.addEventListener("click", function() {
+	ctx.drawImage(img, 0, 0);
+	var idata = ctx.getImageData(0, 0, 1000, 500);
+	var idataf = blwh(idata);
+	ctx.putImageData(idataf, 0, 0);
+	k=1;
+});
+
+neg.addEventListener("click", function() {
+	ctx.drawImage(img, 0, 0);
+	var idata = ctx.getImageData(0, 0, 1000, 500);
+	var idataf = negativ(idata);
+	ctx.putImageData(idataf, 0, 0);
+	k=2;
+});
+
+cr.addEventListener("click", function() {
+	ctx.drawImage(img, 0, 0);
+	var idata = ctx.getImageData(0, 0, 1000, 500);
+	var idataf = coar(idata);
+	ctx.putImageData(idataf, 0, 0);
+	k=3;
+});
+
+function blwh(data) {
+	var px = data.data;
+	for (var i=0; i < px.length; i+=4) 
+	{
+		var r = px[i];
+		var g = px[i+1];
+		var b = px[i+2];
+		px[i] = (r+g+b)/3;
+		px[i+1] = (r+g+b)/3; 
+		px[i+2] = (r+g+b)/3;
+	}
+	return data;
+}
+
+function coar(data) {
+	var px = data.data;
+	var v = 30;
+	for(var i=0; i < px.length; i+=4) 
+	{
+		var r = px[i];
+		var g = px[i+1];
+		var b = px[i+2];
+		px[i] = r%v;
+		px[i+1] = g%v;
+		px[i+2] = b%v; 
+	}
+	return data;
+}
+
+function negativ(data) {
+	var px = data.data;
+	for (var i=0; i < px.length; i+=4) 
+	{
+		var r = px[i];
+		var g = px[i+1];
+		var b = px[i+2];
+		px[i] = 255-(r+g+b);
+		px[i+1] = 255-(r+g+b);
+		px[i+2] = 255-(r+g+b);
+	}
+	return data;
+}
+
